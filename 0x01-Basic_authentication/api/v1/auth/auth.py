df68e5c6cfa5376027e898d17bc3a2aa883b1a4e
Emmanuel Nwachukwu <emmax0121@gmail.com>
2024-08-05 18:22:27 +0000

    auth exclusiion

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,7 +10,10 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Public method require auth of bool retun type """
-        return False
+        if not path or not excluded_paths or path not in excluded_paths:
+            return True 
+        if path in excluded_paths or "/api/v1/status/" in excluded_paths:
+            return False
     
     def authorization_header(self, request=None) -> str:
         """Returns None"""
